fe09eec640c81f6eaa6b44677e2ef1620e9d6a18
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2023-01-24 13:04:41 -0300

    Add typing animation

---
+++ script/main.js
@@ -0,0 +1,7 @@
+/* =================================== Typing animation =================================== */
+const typed = new Typed(".typing", {
+    strings: ["Web Developer", "Frontend Engineer", "Backend Engineer", "Computer Science Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
